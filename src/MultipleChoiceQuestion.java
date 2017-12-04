3cb1dfee498b618e421ba0dca8131b526a92f1c0
lilytran <lilytran@lilys-macbook.socal.rr.com>
2017-12-04 10:15:01 -0800

    added quizzes again

--- src/MultipleChoiceQuestion.java
+++ src/MultipleChoiceQuestion.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+public class MultipleChoiceQuestion extends Question {
 
-public class MultipleChoiceQuestion {
+    ArrayList<String> possibleAnswers = new ArrayList<>();
 
+    public MultipleChoiceQuestion (String question, String answer, ArrayList<String> possibleAnswers){
+        super(question, answer);
+        this.possibleAnswers = possibleAnswers;
+    }
+
+    //sup
+    @Override
+    public String askQuestion() {
+        String ret = "";
+        ret += super.askQuestion() + "\n";
+        for(int i = 0; i < possibleAnswers.size(); i++){
+            ret += possibleAnswers.get(i) + "\n";
+        }
+        return ret;
+    }
+
+    @Override
+    public MultipleChoiceQuestion clone(){
+        return new MultipleChoiceQuestion(super.askQuestion(), super.getAnswer(), possibleAnswers);
+    }
+    public void setPossibleAnswers (ArrayList<String> possibleAnswers){
+        this.possibleAnswers = possibleAnswers;
+    }
+    public ArrayList<String> getPossibleAnswers(){
+        return possibleAnswers;
+    }
+
+    @Override
+    public String toString(){
+        return askQuestion();
+    }
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,46 @@
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Quiz {
+    ArrayList<Question> holdQuestions = new ArrayList<>();
+        //private Question[] holdQuestions;
+        //private int index;
+        private String filename;
 
+        // Modify the Quiz class’ constructor to acnept a filename that will be used in the save method described below.
+    public Quiz (int numQuestions, String filename){
+            //holdQuestions = new Question[numQuestions];
+            holdQuestions = new ArrayList<>();
+            this.filename = filename;
+        }
+
+        // save method, save the question text (and possible answers if MCQ) of all questions associated with that quiz into a file
+        // The filename provided to the constructor will be used as the filename for saving a quiz.
+    public void save (){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))){
+            bw.write(toString());
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    //sup
+    // addQuestion method receives Question object, add to array of questions in the quiz
+    public void addQuestion (Question question){
+        holdQuestions.add(question);
+        //holdQuestions[index] = question;
+        //index++;
+    }
+
+    //Override the toString method in Quiz so that it returns a String containing the text from all questions in the quiz.
+    @Override
+    public String toString(){
+        String ret = "";
+        for (int i = 0; i < holdQuestions.size(); i++) {
+            ret += (i+1) + ". " + holdQuestions.get(i) + "\n";
+        }
+        return ret;
+    }
 }
